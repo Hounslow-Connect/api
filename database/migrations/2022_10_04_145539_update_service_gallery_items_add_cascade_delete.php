9103c20f79f2259c7cb61a5ba3790fe2b7177f53
Stuart Laverick <stuart@appropriatesolutions.co.uk>
2022-10-04 16:23:54 +0100

    Fix style

--- database/migrations/2022_10_04_145539_update_service_gallery_items_add_cascade_delete.php
+++ database/migrations/2022_10_04_145539_update_service_gallery_items_add_cascade_delete.php
@@ -1,15 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class UpdateServiceGalleryItemsAddCascadeDelete extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
@@ -27,8 +25,6 @@ class UpdateServiceGalleryItemsAddCascadeDelete extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
